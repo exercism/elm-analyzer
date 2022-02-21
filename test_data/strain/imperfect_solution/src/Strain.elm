a97a1aa0984510d56bb491957dfba8d8fb42d729
Jérémie Gillet <jie.gillet@gmail.com>
2022-02-21 11:43:21 +0900

    Add comment to test data

--- test_data/strain/imperfect_solution/src/Strain.elm
+++ test_data/strain/imperfect_solution/src/Strain.elm
@@ -5,9 +5,11 @@ import List exposing (filter)
 
 keep : (a -> Bool) -> List a -> List a
 keep predicate =
+-- filter is forbidden
     filter predicate
 
 
 discard : (a -> Bool) -> List a -> List a
 discard predicate =
+-- filter is forbidden
     filter (predicate >> not)

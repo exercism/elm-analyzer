916a9823e5400c016c6edcd62ce40769b4b3a5a5
Jérémie Gillet <jie.gillet@gmail.com>
2022-03-02 20:02:53 +0900

    Add comments to check_files.sh

--- bin/check_files.sh
+++ bin/check_files.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+#
+#  This script is called by bin/smoke_test.hs
+#
+#  It checks that for a given exercise path (given as argument)
+#    1. The analyzer created an output file
+#    2. There is a reference file available for comparison
+#    3. Both files are identical
+#
+#  Otherwise, it fails with an error message.
+#
 
 set -euo pipefail
 

634f4ef7794682291ac0d56d523a3c6fee1f9725
Jeremie Gillet <jie.gillet@gmail.com>
2023-10-28 14:57:13 +0900

    Add rule for practice exercise `custom-set`

--- src/ReviewConfig.elm
+++ src/ReviewConfig.elm
@@ -4,6 +4,7 @@ import Common.NoUnused
 import Common.Simplify
 import Exercise.BettysBikeShop
 import Exercise.BlorkemonCards
+import Exercise.CustomSet
 import Exercise.MariosMarvellousLasagna
 import Exercise.MazeMaker
 import Exercise.Strain
@@ -29,6 +30,7 @@ ruleConfigs =
 
     -- Practice Exercises
     , Exercise.Strain.ruleConfig
+    , Exercise.CustomSet.ruleConfig
     ]
 
 
